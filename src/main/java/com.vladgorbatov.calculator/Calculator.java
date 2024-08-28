0a0487359247b35d56d45cf53dd81ae91559416c
Vlad_Gorbatov <vlad.gorbatov2016@yandex.ru>
2024-08-28 11:47:41 +0300

    added — Endless calculator — Optimization for history — Method start() for class Calculator

--- src/main/java/com.vladgorbatov.calculator/Calculator.java
+++ src/main/java/com.vladgorbatov.calculator/Calculator.java
@@ -1,25 +1,52 @@
 package main.java.com.vladgorbatov.calculator;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Calculator {
-    HashMap<String, Double> history = new HashMap<String, Double>();
+    Map<String, Double> history = new HashMap<>();
     Scanner sc = new Scanner(System.in);
 
-    public void calculate() {
+    public void start() {
+        System.out.println("Привет, это простейший калькулятор!\n" +
+                "— Для решения примера нажмите 1;\n" +
+                "— Для запроса истории решений нажмите 2;\n" +
+                "— Для закрытия калькулятора нажмите 0.\n\n");
 
-        System.out.print("Привет, это простейший калькулятор!\nВведите первое значение: ");
+        while (true) {
+            System.out.print("Введите команду: ");
+            char choice = sc.next().charAt(0);
+
+            switch (choice) {
+                case '1':
+                    startCalculations();
+                    break;
+                case '2':
+                    showHistory();
+                    break;
+                case '0':
+                    stop();
+                    return;
+                default:
+                    System.out.println("Неизвестная команда!\n" +
+                            "— Для решения примера нажмите 1;\n" +
+                            "— Для запроса истории решений нажмите 2;\n" +
+                            "— Для закрытия калькулятора нажмите 0.\n");
+                    break;
+            }
+        }
+    }
+
+    public void startCalculations() {
+        System.out.print("Введите первое значение: ");
         Double a = sc.nextDouble();
-
 
         System.out.print("Введите арифметическую операцию (+, -, *, /): ");
         char operator = sc.next().charAt(0);
 
-
         System.out.print("Введите второе значение: ");
         Double b = sc.nextDouble();
-
 
         double result;
 
@@ -47,27 +74,30 @@ public class Calculator {
 
             default:
                 System.out.println("Ошибка операции! Введите +, -, *, /");
+
                 return;
         }
 
-        String expression = String.format("%.2f %c %.2f",a, operator, b);
+        String expression = String.format("%.2f %c %.2f", a, operator, b);
         history.put(expression, result);
 
         System.out.println("Результат операции: " + result + "\n");
-
     }
 
     public void showHistory() {
-
-        System.out.println("История вычислений: ");
-        for (String expression : history.keySet()) {
-            System.out.println(expression + " = " + history.get(expression));
+        if (history.isEmpty()) {
+            System.out.println("История вычислений пуста.\n");
+        } else {
+            System.out.println("История вычислений: ");
+            for (String expression : history.keySet()) {
+                System.out.println(expression + " = " + history.get(expression));
+            }
+            System.out.println();
         }
-
     }
 
-    public void close(){
+    public void stop() {
         sc.close();
+        System.out.println("Калькулятор завершил работу.");
     }
-
 }
--- src/main/java/com.vladgorbatov.calculator/Main.java
+++ src/main/java/com.vladgorbatov.calculator/Main.java
@@ -3,11 +3,6 @@ package main.java.com.vladgorbatov.calculator;
 public class Main {
     public static void main(String[] args) {
         Calculator calculater = new Calculator();
-
-        calculater.calculate();
-        calculater.calculate();
-        calculater.showHistory();
-        calculater.close();
-
+        calculater.start();
     }
 }
